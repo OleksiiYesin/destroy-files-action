c7e0be4f8f8edec5b1baacc9df329c99dda09874
Oleksii Yesin <Oleksii_Yesin@epam.com>
2022-08-16 13:39:49 +0300

    Changed index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ const core = require('@actions/core');
 const fs = require('fs');
 const fsExtra = require('fs-extra');
 
-const dir = core.getInput('dir', {default: './'});
-
 async function run() {
     try {
+        const dir = core.getInput('dir', {default: './'});
         await fs.readdir(dir, (err, files) => {
             if(err) throw err
             core.info(`You can delete: ${files}`);
